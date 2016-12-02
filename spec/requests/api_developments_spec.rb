a8894db0586288c253d24a117b3a7bdb3f8ac9a6
jim <ejavaguy@gmail.com>
2016-12-01 23:22:12 -0500

    added rspec tests

--- spec/requests/api_developments_spec.rb
+++ spec/requests/api_developments_spec.rb
@@ -1,9 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "ApiDevelopments", type: :request do
+  def parsed_body
+    JSON.parse(response.body)
+  end
+
   describe "RDBMS-backed" do
-    it "create RDBMS-backed model"
-    it "expose RDBMS-backed API resource"
+    before(:each) { Foo.delete_all }
+    after(:each)  { Foo.delete_all }
+
+    it "create RDBMS-backed model" do
+      object=Foo.create(:name=>"test")
+      expect(Foo.find(object.id).name).to eq("test")
+    end
+
+    it "expose RDBMS-backed API resource" do
+      object=Foo.create(:name=>"test")
+      expect(foos_path).to eq("/api/foos")
+      get foo_path(object.id)
+      expect(response).to have_http_status(:ok)
+      expect(parsed_body["name"]).to eq("test")
+    end
   end
 
   describe "MongoDB-backed" do
