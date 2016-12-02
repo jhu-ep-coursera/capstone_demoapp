a0bcfdd15c10aab17b5b7e419c8db23c535a3cdf
jim <ejavaguy@gmail.com>
2016-12-02 17:13:53 -0500

    have mongoid passing

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,34 +1,26 @@
 class BarsController < ApplicationController
   before_action :set_bar, only: [:show, :update, :destroy]
 
-  # GET /bars
-  # GET /bars.json
   def index
     @bars = Bar.all
-
-    render json: @bars
+    #render json: @bars
   end
 
-  # GET /bars/1
-  # GET /bars/1.json
   def show
-    render json: @bar
+    #render json: @bar
   end
 
-  # POST /bars
-  # POST /bars.json
   def create
     @bar = Bar.new(bar_params)
 
     if @bar.save
-      render json: @bar, status: :created, location: @bar
+      #render json: @bar, status: :created, location: @bar
+      render :show, status: :created, location: @bar
     else
       render json: @bar.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /bars/1
-  # PATCH/PUT /bars/1.json
   def update
     @bar = Bar.find(params[:id])
 
@@ -39,8 +31,6 @@ class BarsController < ApplicationController
     end
   end
 
-  # DELETE /bars/1
-  # DELETE /bars/1.json
   def destroy
     @bar.destroy
 
--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,4 +1,5 @@
 class Bar
   include Mongoid::Document
+  include Mongoid::Timestamps
   field :name, type: String
 end
