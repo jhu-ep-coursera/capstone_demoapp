d32a3e3b689dfd0763ca0e2f90fcab2bea03dfea
jim <ejavaguy@gmail.com>
2016-12-01 21:07:44 -0500

    added rspec requirements

---
+++ spec/requests/api_developments_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "ApiDevelopments", type: :request do
+  describe "RDBMS-backed" do
+    it "create RDBMS-backed model"
+    it "expose RDBMS-backed API resource"
+  end
+
+  describe "MongoDB-backed" do
+    it "create MongoDB-backed model"
+    it "expose MongoDB-backed API resource"
+  end
+end
