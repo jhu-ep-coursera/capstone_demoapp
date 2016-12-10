c349973f628a9b555b8305e7f319131e348f2f41
jim <ejavaguy@gmail.com>
2016-12-10 17:57:19 -0500

    added re-direct for client-assets

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     resources :bars, except: [:new, :edit]
   end      
 
+  get "/client-assets/:name.:format", :to => redirect("/client/client-assets/%{name}.%{format}")
+
   get '/ui'  => 'ui#index'
   get '/ui#' => 'ui#index'
   root "ui#index"
