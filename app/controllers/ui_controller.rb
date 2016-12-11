6f460e0bad64f4e00be53e60ec3e09e618ec1b93
jim <ejavaguy@gmail.com>
2016-12-11 18:18:14 -0500

    added redirect

--- app/controllers/ui_controller.rb
+++ app/controllers/ui_controller.rb
@@ -1,2 +1,6 @@
 class UiController < ApplicationController
+
+#  def index
+#    redirect_to "/client/index.html", :status=>:moved_permanently
+#  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   end      
 
   get "/client-assets/:name.:format", :to => redirect("/client/client-assets/%{name}.%{format}")
+  get "/", :to => redirect("/client/index.html")
 
-  get '/ui'  => 'ui#index'
-  get '/ui#' => 'ui#index'
-  root "ui#index"
+#  get '/ui'  => 'ui#index'
+#  get '/ui#' => 'ui#index'
+#  root "ui#index"
 end
