401de06044cb982ddf7ea2894111f3c7172dfa9d
jim <ejavaguy@gmail.com>
2016-12-12 02:56:13 -0500

    added basic spa-app

--- app/assets/javascripts/spa-demo.js
+++ app/assets/javascripts/spa-demo.js
@@ -4,3 +4,7 @@
 //= require angular
 //= require angular-ui-router
 //= require angular-resource
+
+//= require spa-demo/app.module
+//= require spa-demo/app.router
+//= require spa-demo/app.constant
---
+++ client/src/javascripts/spa-demo/app.router.js
@@ -0,0 +1,23 @@
+(function() {
+  "use strict";
+
+  angular
+    .module("spa-demo")
+    .config(RouterFunction);
+
+  RouterFunction.$inject = ["$stateProvider",
+                            "$urlRouterProvider", 
+                            "spa-demo.APP_CONFIG"];
+
+  function RouterFunction($stateProvider, $urlRouterProvider, APP_CONFIG) {
+    $stateProvider
+    .state("home",{
+      url: "/",
+      templateUrl: APP_CONFIG.main_page_html,
+      // controller: ,
+      // controllerAs: ,
+    })
+
+    $urlRouterProvider.otherwise("/");
+  }
+})();
