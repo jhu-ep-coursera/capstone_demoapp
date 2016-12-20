ea2b78012ed5fb629830162ba312eab66c07aa88
jim <ejavaguy@gmail.com>
2016-12-20 12:59:57 -0500

    separate examples with error

--- spec/models/foo_spec.rb
+++ spec/models/foo_spec.rb
@@ -3,17 +3,16 @@ require 'rails_helper'
 describe Foo, type: :model do
 
   context "created Foo" do
-    before(:each) do
-      @foo=Foo.create(:name => "test")
-    end
-    after(:each) do
-      @foo.delete
-    end
+    let(:foo) { Foo.create(:name => "test") }
 
-    it "will be persisted, have a name, and be found" do
-      expect(@foo).to be_persisted
-      expect(@foo.name).to eq("test")
-      expect(Foo.find(@foo.id)).to_not be_nil
+    it "will be persisted" do
+      expect(foo).to_not be_persisted   #causing an error
+    end
+    it "will have a name" do
+      expect(foo.name).to eq("test")
+    end
+    it "will be found" do
+      expect(Foo.find(foo.id)).to_not be_nil
     end
   end
 end
