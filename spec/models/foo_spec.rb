569faedec41bad1f22f1fdff5dce2d4336da260d
jim <ejavaguy@gmail.com>
2016-12-20 12:48:42 -0500

    foo before/after blocks

--- spec/models/foo_spec.rb
+++ spec/models/foo_spec.rb
@@ -2,10 +2,18 @@ require 'rails_helper'
 
 describe Foo, type: :model do
 
-  it "created Foo will be persisted, have a name, and be found" do
-    foo=Foo.create(:name => "test");
-    expect(foo).to be_persisted
-    expect(foo.name).to eq("test")
-    expect(Foo.find(foo.id)).to_not be_nil
+  context "created Foo" do
+    before(:each) do
+      @foo=Foo.create(:name => "test")
+    end
+    after(:each) do
+      @foo.delete
+    end
+
+    it "will be persisted, have a name, and be found" do
+      expect(@foo).to be_persisted
+      expect(@foo.name).to eq("test")
+      expect(Foo.find(@foo.id)).to_not be_nil
+    end
   end
 end
