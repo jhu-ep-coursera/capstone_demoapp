13d476f701464417d80cbb6df7f6761955e67cec
jim <ejavaguy@gmail.com>
2016-12-20 13:04:40 -0500

    it blocks

--- spec/models/foo_spec.rb
+++ spec/models/foo_spec.rb
@@ -5,14 +5,8 @@ describe Foo, type: :model do
   context "created Foo" do
     let(:foo) { Foo.create(:name => "test") }
 
-    it "will be persisted" do
-      expect(foo).to_not be_persisted   #causing an error
-    end
-    it "will have a name" do
-      expect(foo.name).to eq("test")
-    end
-    it "will be found" do
-      expect(Foo.find(foo.id)).to_not be_nil
-    end
+    it { expect(foo).to be_persisted }
+    it { expect(foo.name).to eq("test") }
+    it { expect(Foo.find(foo.id)).to_not be_nil }
   end
 end
