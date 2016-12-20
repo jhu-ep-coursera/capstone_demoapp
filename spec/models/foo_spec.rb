46790bcce2ae4d1271425654c5d158e7ace67844
jim <ejavaguy@gmail.com>
2016-12-20 13:19:13 -0500

    pending examples

--- spec/models/foo_spec.rb
+++ spec/models/foo_spec.rb
@@ -18,4 +18,15 @@ describe Foo, type: :model do
     it { expect(Foo.find(subject.id)).to_not be_nil }
   end
 
+  context "created Foo (lazy)" do
+    let(:before_count) { Foo.count }
+    let(:foo)          { Foo.create(:name => "test") }
+
+    it { expect(foo).to be_persisted }
+    it { expect(foo.name).to eq("test") }
+    it { expect(Foo.find(foo.id)).to_not be_nil }
+    xit { expect(Foo.count).to eq(before_count + 1) }
+    pending { expect(Foo.count).to eq(before_count + 1) }
+    it "will have an additional row"
+  end
 end
