9c9e7d51ffed8418aa7c582f52da4c907fd80101
jim <ejavaguy@gmail.com>
2016-12-20 14:24:04 -0500

    eager let

--- spec/models/foo_spec.rb
+++ spec/models/foo_spec.rb
@@ -18,15 +18,13 @@ describe Foo, type: :model do
     it { expect(Foo.find(subject.id)).to_not be_nil }
   end
 
-  context "created Foo (lazy)" do
-    let(:before_count) { Foo.count }
+  context "created Foo (eager)" do
+    let!(:before_count) { Foo.count }
     let(:foo)          { Foo.create(:name => "test") }
 
     it { expect(foo).to be_persisted }
     it { expect(foo.name).to eq("test") }
     it { expect(Foo.find(foo.id)).to_not be_nil }
-    xit { expect(Foo.count).to eq(before_count + 1) }
-    pending { expect(Foo.count).to eq(before_count + 1) }
-    it "will have an additional row"
+    it { foo; expect(Foo.count).to eq(before_count + 1) }
   end
 end
