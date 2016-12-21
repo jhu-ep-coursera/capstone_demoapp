cd85f9dc7faddd67f0ab9316c9fbf136b59d4c9d
jim <ejavaguy@gmail.com>
2016-12-21 16:50:03 -0500

    query-only cleanup strategy

--- spec/models/foo_spec.rb
+++ spec/models/foo_spec.rb
@@ -2,31 +2,22 @@ require 'rails_helper'
 
 describe Foo, type: :model do
   include_context "db_cleanup", :transaction
-  include_context "db_scope"
+  before(:all) do
+    @foo=Foo.create(:name => "test")
+  end
+  let(:foo) { Foo.find(@foo.id) }
 
   context "created Foo (let)" do
-    let(:foo) { Foo.create(:name => "test") }
-
     it { expect(foo).to be_persisted }
     it { expect(foo.name).to eq("test") }
     it { expect(Foo.find(foo.id)).to_not be_nil }
   end
 
   context "created Foo (subject)" do
-    subject { Foo.create(:name => "test") }
+    subject { @foo }
 
     it { is_expected.to be_persisted }
     it { expect(subject.name).to eq("test") }
     it { expect(Foo.find(subject.id)).to_not be_nil }
-  end
-
-  context "created Foo (eager)" do
-    let!(:before_count) { Foo.count }
-    let(:foo)          { Foo.create(:name => "test") }
-
-    it { expect(foo).to be_persisted }
-    it { expect(foo.name).to eq("test") }
-    it { expect(Foo.find(foo.id)).to_not be_nil }
-    it { foo; expect(Foo.count).to eq(before_count + 1) }
   end
 end
