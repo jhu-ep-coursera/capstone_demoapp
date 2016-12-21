f7e951f8c6d8a86c12f706c5856d3660fe9cc87c
jim <ejavaguy@gmail.com>
2016-12-21 00:36:41 -0500

    mongoid model with cleanup

--- spec/models/bar_spec.rb
+++ spec/models/bar_spec.rb
@@ -4,9 +4,15 @@ require 'mongo'
 Mongo::Logger.logger.level = ::Logger::DEBUG
 
 describe Bar, :type=>:model, :orm=>:mongoid do
+  before(:all) do
+    Bar.delete_all
+  end
 
   context "created Bar (let)" do
     let(:bar) { Bar.create(:name => "test") }
+    after(:each) do
+      bar.delete
+    end
 
     it { expect(bar).to be_persisted }
     it { expect(bar.name).to eq("test") }
