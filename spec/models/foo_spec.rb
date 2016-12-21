accf336063797fceef361db8dc09530db8ffab37
jim <ejavaguy@gmail.com>
2016-12-21 14:57:24 -0500

    added db_cleaner tasks to foo

--- spec/models/foo_spec.rb
+++ spec/models/foo_spec.rb
@@ -1,6 +1,19 @@
 require 'rails_helper'
 
 describe Foo, type: :model do
+  before(:all) do
+    DatabaseCleaner[:active_record].strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  after(:all) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  before(:each) do
+    DatabaseCleaner.start
+  end
+  after(:each) do
+    DatabaseCleaner.clean
+  end
 
   context "created Foo (let)" do
     let(:foo) { Foo.create(:name => "test") }
