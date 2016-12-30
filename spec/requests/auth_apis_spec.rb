a698b3111decc2304a0ac9977ad09477a99e9715
jim <ejavaguy@gmail.com>
2016-12-30 11:59:06 -0500

    signup missing information

--- spec/requests/auth_apis_spec.rb
+++ spec/requests/auth_apis_spec.rb
@@ -7,9 +7,7 @@ RSpec.describe "Authentication Api", type: :request do
   context "sign-up" do
     context "valid registration" do
       it "successfully creates account" do
-        jpost user_registration_path, user_props
-        #pp parsed_body
-        expect(response).to have_http_status(:ok)
+        signup user_props
 
         payload=parsed_body
         expect(payload).to include("status"=>"success")
@@ -25,7 +23,19 @@ RSpec.describe "Authentication Api", type: :request do
 
     context "invalid registration" do
       context "missing information" do
-        it "reports error with messages"
+        it "reports error with messages" do
+          signup user_props.except(:email), :unprocessable_entity
+          #pp parsed_body
+
+          payload=parsed_body
+          expect(payload).to include("status"=>"error")
+          expect(payload).to include("data")
+          expect(payload["data"]).to include("email"=>nil)
+          expect(payload).to include("errors")
+          expect(payload["errors"]).to include("email")
+          expect(payload["errors"]).to include("full_messages")
+          expect(payload["errors"]["full_messages"]).to include(/Email/i)
+        end
       end
 
       context "non-unique information" do
--- spec/support/api_helper.rb
+++ spec/support/api_helper.rb
@@ -10,6 +10,11 @@ module ApiHelper
       self.send(http_method_name, path, params.to_json, headers)
     end
   end
+
+  def signup registration, status=:ok
+    jpost user_registration_path, registration
+    expect(response).to have_http_status(status)
+  end
 end
 
 RSpec.shared_examples "resource index" do |model|
