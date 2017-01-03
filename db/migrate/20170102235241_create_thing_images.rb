e715d607efe0a550581ad1b8c3628b8897025114
jim <ejavaguy@gmail.com>
2017-01-03 03:46:03 -0500

    merged migration files

--- db/migrate/20170102235241_create_thing_images.rb
+++ db/migrate/20170102235241_create_thing_images.rb
@@ -1,5 +1,20 @@
 class CreateThingImages < ActiveRecord::Migration
   def change
+    create_table :images do |t|
+      t.string :caption
+      t.integer :creator_id, {null:false}
+
+      t.timestamps null: false
+    end
+
+    create_table :things do |t|
+      t.string :name, {null: false}
+      t.text :description
+      t.text :notes
+
+      t.timestamps null: false
+    end
+
     create_table :thing_images do |t|
       t.references :image, {index: true, foreign_key: true, null:false}
       t.references :thing, {index: true, foreign_key: true, null:false}
@@ -8,6 +23,9 @@ class CreateThingImages < ActiveRecord::Migration
 
       t.timestamps null: false
     end
+
+    add_index :images, :creator_id
+    add_index :things, :name
     add_index :thing_images, [:image_id, :thing_id], unique:true
   end
 end
