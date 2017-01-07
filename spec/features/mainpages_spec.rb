3b5e2cd211d1d7b7af5b44aa49a97bdedf2d1253
jim <ejavaguy@gmail.com>
2017-01-07 03:45:11 -0500

    turned off mainpage specs

--- spec/features/mainpages_spec.rb
+++ spec/features/mainpages_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Mainpages", type: :feature, :js=>true do
+RSpec.xfeature "Mainpages", type: :feature, :js=>true do
 
   context "when the main page is accessed" do
     before(:each) do
