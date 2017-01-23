d62514e7271ede83c0247c34599bbf82b1a9f34f
jim <ejavaguy@gmail.com>
2017-01-23 03:02:04 -0500

    can create link image to things

--- app/assets/javascripts/spa-demo/subjects/images/images.component.js
+++ app/assets/javascripts/spa-demo/subjects/images/images.component.js
@@ -100,13 +100,27 @@
 
     function update() {
       vm.item.errors = null;
-      vm.item.$update().then(
-        function(){ 
-          console.log("update complete", vm.item);
+      var update=vm.item.$update();
+      linkThings(update);
+    }
+
+    function linkThings(parentPromise) {
+      var promises=[]
+      if (parentPromise) { promises.push(parentPromise); }
+      angular.forEach(vm.selected_linkables, function(linkable){
+        var resource=ImageThing.save({image_id:vm.item.id}, {thing_id:linkable});
+        promises.push(resource.$promise);
+      });
+
+      vm.selected_linkables=[];
+      console.log("waiting for promises", promises);
+      $q.all(promises).then(
+        function(response){
+          console.log("promise.all response", response); 
           $scope.imageform.$setPristine();
-          $state.reload(); 
+          reload(); 
         },
-        handleError);      
+        handleError);    
     }
 
     function remove() {
