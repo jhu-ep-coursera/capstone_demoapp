e95be083c8ed40050d40388f5f2c8338702f24ca
jim <ejavaguy@gmail.com>
2017-02-01 23:46:14 -0500

    changed to inner join to enforce having role

--- app/policies/thing_policy.rb
+++ app/policies/thing_policy.rb
@@ -20,11 +20,12 @@ class ThingPolicy < ApplicationPolicy
   end
   class Scope < Scope
     def user_roles 
-      joins_clause=["left join Roles r on r.mname='Thing'",
+      joins_clause=["join Roles r on r.mname='Thing'",
                     "r.mid=Things.id",
                     "r.user_id #{user_criteria}"].join(" and ")
       scope.select("Things.*, r.role_name")
            .joins(joins_clause)
+           .where("r.role_name"=>[Role::ORGANIZER, Role::MEMBER])
     end
     def resolve
       user_roles 
