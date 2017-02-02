6b474f062a4add4aa80a464de01ceed7a4217ad2
jim <ejavaguy@gmail.com>
2017-02-01 23:23:35 -0500

    added thing authz policy defs

--- app/policies/thing_policy.rb
+++ app/policies/thing_policy.rb
@@ -1,4 +1,23 @@
 class ThingPolicy < ApplicationPolicy
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def create?
+    originator?
+  end
+
+  def update?
+    organizer?
+  end
+
+  def destroy?
+    organizer_or_admin?
+  end
   class Scope < Scope
     def resolve
       scope
