29024cea058a3de47765d52c1a8d9ef38a4ac7a5
jim <ejavaguy@gmail.com>
2017-02-11 16:56:38 -0500

    assign image content spec

--- spec/models/image_content_spec.rb
+++ spec/models/image_content_spec.rb
@@ -10,20 +10,57 @@ RSpec.describe "ImageContent", type: :model do
 
   context "BSON::Binary" do
     it "demonstrates BSON::Binary" do
-      fin
+      binary=BSON::Binary.new(fin.read)
+      expect(binary.data.size).to eq(fin.size)
+
+      fout=File.open("tmp/out.jpg", "wb")
+      fout.write(binary.data)
+      expect(fout.size).to eq(fin.size)
+      fout.close
     end
 
     context "using helper" do
-      it "derives BSON::Binary from file"
-      it "derives BSON::Binary from StringIO"
-      it "derives BSON::Binary from BSON::Binary"
-      it "derives BSON::Binary from base64 encoded String"
+      it "derives BSON::Binary from file" do
+        binary=ImageContent.to_binary(fin)
+        expect(binary.class).to be(BSON::Binary)
+        expect(binary.data.size).to eq(fin.size)
+      end
+      it "derives BSON::Binary from StringIO" do
+        strio=StringIO.new(fin.read)
+        binary=ImageContent.to_binary(strio)
+        expect(binary.class).to be(BSON::Binary)
+        expect(binary.data.size).to eq(fin.size)
+      end
+      it "derives BSON::Binary from BSON::Binary" do
+        binary=BSON::Binary.new(fin.read)
+        binary=ImageContent.to_binary(binary)
+        expect(binary.class).to be(BSON::Binary)
+        expect(binary.data.size).to eq(fin.size)
+      end
+      it "derives BSON::Binary from base64 encoded String" do
+        content=BSON::Binary.new(fin.read)
+        b64=Base64.encode64(content.data)
+        binary=ImageContent.to_binary(b64)
+        expect(binary.class).to be(BSON::Binary)
+        expect(binary.data.size).to eq(fin.size)
+      end
     end
   end
 
   context "assign content" do
-    it "sets content"
-    it "mass-assigns content"
+    it "sets content" do
+      strio=StringIO.new(fin.read)
+      ic=ImageContent.new
+      ic.content=strio
+      expect(ic.content.class).to be(BSON::Binary)
+      expect(ic.content.data.size).to eq(fin.size)
+    end
+    it "mass-assigns content" do
+      strio=StringIO.new(fin.read)
+      ic=ImageContent.new(:content=>strio)
+      expect(ic.content.class).to be(BSON::Binary)
+      expect(ic.content.data.size).to eq(fin.size)
+    end
   end
 
   context "set size from JPEG" do
