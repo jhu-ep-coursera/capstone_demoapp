7dcfc9a03e997241c94a3b58a4fa8eb81637641d
jim <ejavaguy@gmail.com>
2017-02-11 16:56:47 -0500

    assign image content impl

--- app/models/image_content.rb
+++ app/models/image_content.rb
@@ -6,4 +6,21 @@ class ImageContent
   field :content_type, type: String
   field :content, type: BSON::Binary
   field :original, type: Mongoid::Boolean
+
+  def content=(value)
+    self[:content]=self.class.to_binary(value)
+  end
+
+  def self.to_binary(value)
+    case
+    when value.is_a?(IO) || value.is_a?(StringIO)
+      value.rewind
+      BSON::Binary.new(value.read)
+    when value.is_a?(BSON::Binary)
+      value
+    when value.is_a?(String)
+      decoded=Base64.decode64(value)
+      BSON::Binary.new(decoded)
+    end
+  end
 end
