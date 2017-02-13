3687985b3da2ea21badbd5fefdf012e3734d2dc4
jim <ejavaguy@gmail.com>
2017-02-12 20:39:41 -0500

    added image/content relationship

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,7 @@
 class Image < ActiveRecord::Base
   include Protectable
+  attr_accessor :image_content
+
   has_many :thing_images, inverse_of: :image, dependent: :destroy
   has_many :things, through: :thing_images
 end
