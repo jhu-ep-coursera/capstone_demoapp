788da98da3b18d11fc992c9c2fe00dd9227dccc5
jim <ejavaguy@gmail.com>
2017-02-17 03:29:11 -0500

    added image size queries

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -20,7 +20,7 @@ class ImagesController < ApplicationController
   end
 
   def content
-    result=ImageContent.image(@image).smallest.first
+    result=ImageContent.image(@image).smallest(params[:width],params[:height]).first
     if result
       options = { type: result.content_type,
                   disposition: "inline",
