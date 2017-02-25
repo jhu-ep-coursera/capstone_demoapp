95e4b1e0ea1c2346b0ae51bb8d93f16d4e3c20f6
jim <ejavaguy@gmail.com>
2017-02-25 02:15:06 -0500

    displays image content

--- app/assets/javascripts/spa-demo/layout/image_viewer/image_viewer.component.js
+++ app/assets/javascripts/spa-demo/layout/image_viewer/image_viewer.component.js
@@ -20,11 +20,30 @@
   ImageViewerController.$inject = ["$scope"];
   function ImageViewerController($scope) {
     var vm=this;
+    vm.imageUrl=imageUrl;
+    vm.imageId=imageId;    
+    vm.isCurrentIndex=isCurrentIndex;
 
     vm.$onInit = function() {
+      vm.currentIndex = 0;      
       console.log(vm.name, "ImageViewerController", $scope);
     }
     return;
     //////////////
+    function isCurrentIndex(index) {
+      return index === vm.currentIndex;
+    }
+
+    function imageUrl(object) {
+      if (!object) { return null; }
+      var url = object.image_id ? object.image_content_url : object.content_url;
+      console.log(vm.name, "url=", url);
+      return url;
+    }
+    function imageId(object) {
+      if (!object) { return null }
+      var id = object.image_id ? object.image_id : object.id;
+      return id; 
+    }  
   }
 })();
