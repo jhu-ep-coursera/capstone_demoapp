74e85d23c3c58847892ef67221d79b78f3881839
jim <ejavaguy@gmail.com>
2017-02-25 03:35:00 -0500

    query sizes implemented

--- app/assets/javascripts/spa-demo/layout/image_query_size.service.js
+++ app/assets/javascripts/spa-demo/layout/image_query_size.service.js
@@ -5,13 +5,31 @@
     .module("spa-demo.layout")
     .factory("spa-demo.layout.ImageQuerySize", ImageQuerySizeFactory);
 
-  ImageQuerySizeFactory.$inject = ["$httpParamSerializer"];
-  function ImageQuerySizeFactory($httpParamSerializer) {
+  ImageQuerySizeFactory.$inject = ["$window","$httpParamSerializer"];
+  function ImageQuerySizeFactory($window, $httpParamSerializer) {
 
     function ImageQuerySize(element, minWidth) {
       this.element = element;
       this.width   = null;
       this.height  = null;
+      this.updateSizes(minWidth);
+    }
+    
+    ImageQuerySize.prototype.updateSizes = function(minWidth) {
+      var w = this.queryWidth(this.element.innerWidth());
+      var h = this.queryHeight(this.element.innerHeight());
+      var newSize = (w != this.width) || (h != this.height);
+      if (newSize) {
+        this.width=w;
+        this.height=h;        
+      }
+      return newSize;
+    }
+    ImageQuerySize.prototype.listen = function(handler) {
+      angular.element($window).on('resize', handler);
+    }
+    ImageQuerySize.prototype.nolisten = function(handler) {
+      angular.element($window).off('resize', handler);
     }
 
     //return complete query string
@@ -24,6 +42,32 @@
         params.height=this.height;
       }
       return Object.keys(params).length==0 ? "" : "?" + $httpParamSerializer(params);
+    }    
+
+    //break width into supported size boundaries
+    ImageQuerySize.prototype.queryWidth = function(width) {
+      var queryWidth=1200;
+      if (width <= 100) {
+        queryWidth=100
+      } else if (width <= 320) {
+        queryWidth=320
+      } else if (width <= 800) {
+        queryWidth=800
+      }
+      return queryWidth;
+    }
+
+    //break height into supported size boundaries
+    ImageQuerySize.prototype.queryHeight = function(height) {
+      var queryHeight=800
+      if (height <= 67) {
+        queryHeight = 67
+      } else if (height <= 213) {
+        queryHeight = 213
+      } else if (height <= 533) {
+        queryHeight = 533
+      }
+      return queryHeight;
     }    
 
     /////
--- app/assets/javascripts/spa-demo/layout/image_viewer/image_viewer.component.js
+++ app/assets/javascripts/spa-demo/layout/image_viewer/image_viewer.component.js
@@ -9,6 +9,7 @@
       bindings: {
         name: "@",
         images: "<",
+        minWidth: "@"
       },
     });
 
@@ -17,26 +18,73 @@
     return APP_CONFIG.image_viewer_html;
   }    
 
-  ImageViewerController.$inject = ["$scope"];
-  function ImageViewerController($scope) {
+  ImageViewerController.$inject = ["$scope", "$element","spa-demo.layout.ImageQuerySize"];
+  function ImageViewerController($scope, $element,ImageQuerySize) {
+    var sizing=null;
     var vm=this;
     vm.imageUrl=imageUrl;
     vm.imageId=imageId;    
+    vm.imageCaption=imageCaption;
     vm.isCurrentIndex=isCurrentIndex;
+    vm.previousImage=previousImage;
+    vm.nextImage=nextImage;
 
     vm.$onInit = function() {
       vm.currentIndex = 0;      
       console.log(vm.name, "ImageViewerController", $scope);
     }
+    vm.$postLink = function() {
+      sizing=new ImageQuerySize($element.find('div'), this.minWidth);
+      vm.queryString=sizing.queryString();
+      sizing.listen(resizeHandler);      
+    }        
+    vm.$onDestroy = function() {
+      sizing.nolisten(resizeHandler);
+    }
     return;
     //////////////
+    function resizeHandler(event) { 
+      console.log("window resized");
+      if (sizing.updateSizes(vm.minWidth)) {
+        vm.queryString=sizing.queryString();
+        console.log("new query size", vm.queryString);
+        $scope.$apply();
+      }
+    }
     function isCurrentIndex(index) {
       return index === vm.currentIndex;
+    }
+    function nextImage() {
+      setCurrentIndex(vm.currentIndex+1);
+    }
+    function previousImage() {
+      setCurrentIndex(vm.currentIndex-1);
+    }
+    function setCurrentIndex(index) {
+      console.log("setCurrentIndex", vm.name, index);
+      if (vm.images && vm.images.length > 0) {
+        if (index >= vm.images.length) {
+          vm.currentIndex=0;
+        } else if (index < 0) {
+          vm.currentIndex=vm.images.length-1;
+        } else {
+          vm.currentIndex=index;
+        }
+      } else {
+        vm.currentIndex=0;
+      }
+    }
+
+    function imageCaption(object) {
+      if (!object) { return null; }
+      var caption = object.image_id ? object.image_caption : object.caption;
+      return caption;
     }
 
     function imageUrl(object) {
       if (!object) { return null; }
       var url = object.image_id ? object.image_content_url : object.content_url;
+      url += vm.queryString;
       console.log(vm.name, "url=", url);
       return url;
     }
