e25149394afae79a91e7c24254e05982a5deb3b7
jim <ejavaguy@gmail.com>
2017-02-26 22:46:10 -0500

    server-side image cropping and marking

--- app/services/image_content_creator.rb
+++ app/services/image_content_creator.rb
@@ -20,12 +20,29 @@ class ImageContentCreator
   def build_size size
     mm_image=MiniMagick::Image.read(@original.content.data)
     mm_image.format "jpg"
-    mm_image.resize size
+    mm_image.resize size+"^"
+    mm_image.combine_options do |c|
+      c.gravity "center"
+      c.crop size+"+0+0"
+    end
     new_contents = StringIO.new
     mm_image.write new_contents
     ImageContent.new(:image_id=>@image.id,
                      :content_type=>"image/jpg", 
                      :content=>new_contents)
+  end
+
+  def self.annotate(text,content) 
+    image = MiniMagick::Image.read(content)
+    image.combine_options do |c|
+      c.pointsize 400
+      c.fill "white"
+      c.gravity "center"
+      c.draw("text 0,0 '#{text}'")
+    end
+    result=StringIO.new
+    image.write(result)
+    result.string
   end
 
   def save!
--- spec/factories/image_contents.rb
+++ spec/factories/image_contents.rb
@@ -3,6 +3,12 @@ FactoryGirl.define do
   factory :image_content do
     content_type "image/jpg"
         #default to the original size when we generate just attributes
-    content { Base64.encode64(ImageContentHelper.sample_image_file.read) }
+    sequence(:content) {|idx|
+      File.open(ImageContentHelper.sample_filepath,"rb") { |f| 
+        image=StringIO.new(f.read)
+        image=ImageContentCreator.annotate(idx, image)
+        Base64.encode64(image)
+      }
+    }
   end
 end
