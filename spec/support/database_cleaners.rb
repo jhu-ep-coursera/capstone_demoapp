f168f59d736e43fea5064261fd009efe26de30a6
jim <ejavaguy@gmail.com>
2017-03-11 10:37:41 -0500

    excluded locations from DB cleaner

--- spec/support/database_cleaners.rb
+++ spec/support/database_cleaners.rb
@@ -2,15 +2,15 @@ require 'database_cleaner'
 
 shared_context "db_cleanup" do |ar_strategy=:truncation|
   before(:all) do
-    DatabaseCleaner[:mongoid].strategy = :truncation
+    DatabaseCleaner[:mongoid].strategy = :truncation, {:except => %w(locations)}
     DatabaseCleaner[:active_record].strategy = ar_strategy
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.clean_with(:truncation, {:except => %w(locations)})
   end
   after(:each, :type=>feature) do
     Capybara.reset_sessions!  #cleared up some random failures
   end
   after(:all) do
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.clean_with(:truncation, {:except => %w(locations)})
   end
 end
 
@@ -25,26 +25,26 @@ end
 
 shared_context "db_clean_after" do
   after(:all) do
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.clean_with(:truncation, {:except => %w(locations)})
   end
 end
 shared_context "db_clean_all" do
   before(:all) do
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.clean_with(:truncation, {:except => %w(locations)})
   end
   after(:all) do
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.clean_with(:truncation, {:except => %w(locations)})
   end
 end
 
 shared_context "db_cleanup_each" do |ar_strategy=:truncation|
   before(:all) do
-    DatabaseCleaner[:mongoid].strategy = :truncation
+    DatabaseCleaner[:mongoid].strategy = :truncation, {:except => %w(locations)}
     DatabaseCleaner[:active_record].strategy = ar_strategy
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.clean_with(:truncation, {:except => %w(locations)})
   end
   after(:all) do
-    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.clean_with(:truncation, {:except => %w(locations)})
   end
   before(:each) do
     DatabaseCleaner.start
