9129f1af691fb8ae4d4cbbbda37b71bfbfacf3e1
jim <ejavaguy@gmail.com>
2017-03-11 11:13:10 -0500

    added position to image controller

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,6 @@
 class ImagesController < ApplicationController
   before_action :set_image, only: [:show, :update, :destroy, :content]
-  wrap_parameters :image, include: ["caption"]
+  wrap_parameters :image, include: ["caption", "position"]
   before_action :authenticate_user!, only: [:create, :update, :destroy]
   after_action :verify_authorized, except: [:content]
   after_action :verify_policy_scoped, only: [:index]
@@ -80,7 +80,7 @@ class ImagesController < ApplicationController
     end
 
     def image_params
-      params.require(:image).permit(:caption)
+      params.require(:image).permit(:caption,:position=>[:lng,:lat])
     end
 
     def image_content_params
