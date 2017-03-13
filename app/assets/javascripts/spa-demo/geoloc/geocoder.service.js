9c35dfb8f93901eb97aad5e7b7e53ad222afbaef
jim <ejavaguy@gmail.com>
2017-03-12 21:36:20 -0400

    added geocode  caching

--- app/assets/javascripts/spa-demo/geoloc/geocoder.service.js
+++ app/assets/javascripts/spa-demo/geoloc/geocoder.service.js
@@ -8,8 +8,12 @@
   Geocoder.$inject = ["$resource", "spa-demo.config.APP_CONFIG"];
 
   function Geocoder($resource, APP_CONFIG) {
-    var addresses = $resource(APP_CONFIG.server_url + "/api/geocoder/addresses",{},{});
-    var positions = $resource(APP_CONFIG.server_url + "/api/geocoder/positions",{},{});
+    var addresses = $resource(APP_CONFIG.server_url + "/api/geocoder/addresses",{},{
+      get: { cache: true }
+    });
+    var positions = $resource(APP_CONFIG.server_url + "/api/geocoder/positions",{},{
+      get: { cache: true }      
+    });
     var service = this;
     service.getLocationByAddress=getLocationByAddress;
     service.getLocationByPosition=getLocationByPosition;
