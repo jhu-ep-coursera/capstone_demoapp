f4e12ec795fdb1abc18e0989a2ab18ccaabc6035
jim <ejavaguy@gmail.com>
2017-03-17 15:03:31 -0400

    added current loc jhu config impl

--- app/assets/javascripts/spa-demo/geoloc/jhu_location.config.js
+++ app/assets/javascripts/spa-demo/geoloc/jhu_location.config.js
@@ -5,7 +5,11 @@
     .module("spa-demo.geoloc")
     .config(JhuLocationOverride);
 
-  JhuLocationOverride.$inject=[];
-  function JhuLocationOverride() {
+  JhuLocationOverride.$inject=["spa-demo.geoloc.myLocationProvider"];
+  function JhuLocationOverride(myLocationProvider) {
+    myLocationProvider.usePositionOverride({
+      longitude:-76.6200464, 
+      latitude: 39.3304957      
+    });
   }
 })();
