61be7630e149173515adef1d2790e2862e20978c
jim <ejavaguy@gmail.com>
2017-03-17 15:27:11 -0400

    added change of style on error

--- app/assets/javascripts/spa-demo/geoloc/current_origin/current_origin_selector.component.js
+++ app/assets/javascripts/spa-demo/geoloc/current_origin/current_origin_selector.component.js
@@ -26,6 +26,7 @@
     vm.clearOrigin=clearOrigin;
     vm.isCurrentLocationSupported = myLocation.isCurrentLocationSupported;
     vm.useCurrentLocation=useCurrentLocation;
+    vm.myPositionError=null;
 
     vm.$onInit = function() {
       console.log("CurrentOriginSelectorController",$scope);
@@ -52,9 +53,11 @@
         function(location){
           console.log("useCurrentLocation", location);
           currentOrigin.setLocation(location);
+          vm.myPositionError=null;
         },
         function(err){
           console.log(err);
+          vm.myPositionError=err;
         });
     }    
 
