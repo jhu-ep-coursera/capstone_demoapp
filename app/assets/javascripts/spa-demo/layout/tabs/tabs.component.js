9b5026cd35c07c39b3488ac9e0a5d03c46087d36
jim <ejavaguy@gmail.com>
2017-03-21 01:13:33 -0400

    added simplistic tab component

--- app/assets/javascripts/spa-demo/layout/tabs/tabs.component.js
+++ app/assets/javascripts/spa-demo/layout/tabs/tabs.component.js
@@ -8,8 +8,17 @@
       controller: TabsController,
       transclude: true,
       //bindings: {},
-    });
-
+    })
+    .component("sdTab", {
+      controller: TabController,
+      bindings: {
+        label: "@"
+      },
+      require: {
+        tabsController: "^^sdTabs"
+      }
+    })
+    ;
 
   tabsTemplateUrl.$inject = ["spa-demo.config.APP_CONFIG"];
   function tabsTemplateUrl(APP_CONFIG) {
@@ -19,9 +28,26 @@
   TabsController.$inject = ["$scope"];
   function TabsController($scope) {
     var vm=this;
+    vm.tabs=[];
 
     vm.$onInit = function() {
       console.log("TabsController",$scope);
+    }
+    return;
+    //////////////
+  }
+  TabsController.prototype.addTab = function(tab) {
+    this.tabs.push(tab);
+  }
+
+
+  TabController.$inject = ["$scope"];
+  function TabController($scope) {
+    var vm=this;
+
+    vm.$onInit = function() {
+      console.log("TabController",$scope);
+      vm.tabsController.addTab(vm);
     }
     return;
     //////////////
