66d2cf7f6ec48279c9e9aec493bc15a9101cd4f0
jim <ejavaguy@gmail.com>
2017-03-21 03:27:49 -0400

    added left/right sides directive

--- app/assets/javascripts/spa-demo/layout/areas/areas.component.js
+++ app/assets/javascripts/spa-demo/layout/areas/areas.component.js
@@ -14,12 +14,25 @@
       controller: AreaController,
       transclude: true,
       bindings: {
-        label: "@"
+        label: "@",
+        position: "@"        
       },
       require: {
         areasController: "^^sdAreas"
       }
     })
+    .directive("sdAreasSide", [function(){
+      return {
+        controller: AreasSideController,
+        controllerAs: "sideVM",
+        bindToController: true,
+        restrict: "A",
+        scope: false,
+        require: {
+          areas: "^sdAreas"
+        }
+      }
+    }])
     ;
 
   areasTemplateUrl.$inject = ["spa-demo.config.APP_CONFIG"];
@@ -35,6 +48,8 @@
   function AreasController($scope) {
     var vm=this;
     vm.areas=[];
+    vm.areasLeft = [];
+    vm.areasRight = [];
 
     vm.$onInit = function() {
       console.log("AreasController",$scope);
@@ -44,7 +59,30 @@
   }
   AreasController.prototype.addArea = function(area) {
     this.areas.push(area);
+    if (area.position==="left") {
+      this.areasLeft.push(area);
+    } else if (area.position==="right") {
+      this.areasRight.push(area);
+    }
   }  
+  AreasController.prototype.getAreas = function(position) {
+    var collection = null;
+    if (position==="left") {
+      collection=this.areasLeft;
+    } else if (position==="right") {
+      collection=this.areasRight;
+    }
+    return collection;
+  }  
+  AreasController.prototype.countActive = function(position) {
+    var collection = this.getAreas(position);
+    var areasActive=0;
+    angular.forEach(collection, function(area){
+      if (area.show) { areasActive += 1; }
+    })
+    //console.log("countActive", collection, areasActive);
+    return areasActive;
+  }
 
 
   AreaController.$inject = ["$scope"];
@@ -58,6 +96,24 @@
     }
     return;
     //////////////
+  }
+
+
+  AreasSideController.$inject = [];
+  function AreasSideController() { 
+    var vm = this;
+    vm.isHidden = isHidden;
+
+    vm.$onInit = function() {
+      console.log("AreasSideController", vm);      
+    }
+    return;
+    /////////////////
+    function isHidden(position) {
+      var result=vm.areas.countActive(position)===0;  
+      console.log("isHidden", position, result);
+      return result;
+    }
   }
 
 })();
