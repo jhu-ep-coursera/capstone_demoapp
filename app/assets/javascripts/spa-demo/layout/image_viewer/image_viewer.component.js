6ad9568e67ce3fd465bd37b7c91c0e633d5ad0cc
jim <ejavaguy@gmail.com>
2017-03-28 02:18:19 -0400

    added completed current image viewer

--- app/assets/javascripts/spa-demo/layout/image_viewer/image_viewer.component.js
+++ app/assets/javascripts/spa-demo/layout/image_viewer/image_viewer.component.js
@@ -9,6 +9,8 @@
       bindings: {
         name: "@",
         images: "<",
+        currentImage: "<currentImageIndex",
+        indexChanged: "&",
         minWidth: "@"
       },
     });
@@ -30,7 +32,7 @@
     vm.nextImage=nextImage;
 
     vm.$onInit = function() {
-      vm.currentIndex = 0;      
+      if (!vm.currentIndex) { vm.currentIndex = 0; }
       console.log(vm.name, "ImageViewerController", $scope);
     }
     vm.$postLink = function() {
@@ -40,6 +42,12 @@
     }        
     vm.$onDestroy = function() {
       sizing.nolisten(resizeHandler);
+    }
+    vm.$onChanges = function(changes) {
+      console.log("$onChanges", vm.name, changes);
+      if (changes.currentImage) {
+        vm.currentIndex = changes.currentImage.currentValue;
+      }
     }
     return;
     //////////////
@@ -62,6 +70,8 @@
     }
     function setCurrentIndex(index) {
       console.log("setCurrentIndex", vm.name, index);
+      var originalValue = vm.currentIndex;
+
       if (vm.images && vm.images.length > 0) {
         if (index >= vm.images.length) {
           vm.currentIndex=0;
@@ -72,6 +82,10 @@
         }
       } else {
         vm.currentIndex=0;
+      }
+
+      if (originalValue !== vm.currentIndex) {
+        vm.indexChanged({index:vm.currentIndex});
       }
     }
 
--- app/assets/javascripts/spa-demo/subjects/current_images/current_images.component.js
+++ app/assets/javascripts/spa-demo/subjects/current_images/current_images.component.js
@@ -6,12 +6,24 @@
     .component("sdCurrentImages", {
       templateUrl: imagesTemplateUrl,
       controller: CurrentImagesController,
-    });
-
+    })
+    .component("sdCurrentImageViewer", {
+      templateUrl: imageViewerTemplateUrl,
+      controller: CurrentImageViewerController,
+      bindings: {
+        name: "@",
+        minWidth: "@"
+      }
+    })
+    ;
 
   imagesTemplateUrl.$inject = ["spa-demo.config.APP_CONFIG"];
   function imagesTemplateUrl(APP_CONFIG) {
     return APP_CONFIG.current_images_html;
+  }    
+  imageViewerTemplateUrl.$inject = ["spa-demo.config.APP_CONFIG"];
+  function imageViewerTemplateUrl(APP_CONFIG) {
+    return APP_CONFIG.current_image_viewer_html;
   }    
 
   CurrentImagesController.$inject = ["$scope",
@@ -36,4 +48,32 @@
       currentSubjects.setCurrentImage(index);
     }
   }
+
+  CurrentImageViewerController.$inject = ["$scope",
+                                          "spa-demo.subjects.currentSubjects"];
+  function CurrentImageViewerController($scope, currentSubjects) {
+    var vm=this;
+    vm.viewerIndexChanged = viewerIndexChanged;
+
+    vm.$onInit = function() {
+      console.log("CurrentImageViewerController",$scope);
+    }
+    vm.$postLink = function() {
+      $scope.$watch(
+        function() { return currentSubjects.getImages(); }, 
+        function(images) { vm.images = images; }
+      );
+      $scope.$watch(
+        function() { return currentSubjects.getCurrentImageIndex(); }, 
+        function(index) { vm.currentImageIndex = index; }
+      );
+    }    
+    return;
+    //////////////
+    function viewerIndexChanged(index) {
+      console.log("viewer index changed, setting currentImage", index);
+      currentSubjects.setCurrentImage(index);
+    }
+  }
+
 })();
