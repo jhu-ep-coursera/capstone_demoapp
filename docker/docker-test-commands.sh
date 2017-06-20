acb542765e0a4738ff7bd84f458bd10571d32892
jim <ejavaguy@gmail.com>
2017-06-19 21:47:48 -0400

    adjusted test script

--- docker/docker-test-commands.sh
+++ docker/docker-test-commands.sh
@@ -4,5 +4,6 @@
 set -x 
 rake db:create RAILS_ENV=test
 rake db:migrate RAILS_ENV=test
-rspec --order random --fail-fast
+#rspec --order random --fail-fast
+rspec spec/features/subject_maps_spec.rb
 #tail -f Gemfile
